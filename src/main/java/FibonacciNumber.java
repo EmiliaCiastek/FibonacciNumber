a721170c881d36967649f7a7b1d63069556ccd47
emcia <emciastek@gmail.com>
2017-11-08 19:07:53 +0100

    - Iterative method - iterative with Stream method - Recursive for negative input

--- src/main/java/FibonacciNumber.java
+++ src/main/java/FibonacciNumber.java
@@ -1,11 +1,40 @@
+import java.util.stream.Stream;
+
 public class FibonacciNumber {
+    // for "negafibonacci" {-8, -21}, {-1, 1}: F(-n) = (-1) ^(n+1) * F(n)
+
     public int calculateRecursive(int inputNumber) {
-        if(inputNumber == 0) {
-            return 0;
-        } else if(inputNumber == 1){
-            return 1;
+        if(inputNumber < 0){
+            return (int) (Math.pow((-1), inputNumber + 1) * calculateRecursive(-1 * inputNumber));
+        } else if(inputNumber < 2) {
+            return inputNumber;
         }
 
         return calculateRecursive(inputNumber - 1)  + calculateRecursive(inputNumber - 2);
+    }
+
+    public int calculateIterative(int inputNumber) {
+        if(inputNumber < 2) {
+            return inputNumber;
+        }
+
+        int fibonacciOfZero = 0;
+        int fibonacciOfOne = 1;
+        int tmp;
+        for (int i=inputNumber ; i>2 ; i--) {
+            tmp = fibonacciOfZero;
+            fibonacciOfZero = fibonacciOfOne;
+            fibonacciOfOne = fibonacciOfOne + tmp;
+        }
+        return fibonacciOfZero + fibonacciOfOne;
+    }
+
+    public int calculateStreams(int inputNumber) {
+
+        return  Stream.iterate(new int[]{ 0, 1 }, fib->new int[]{fib[1], fib[0]+fib[1]})
+                .limit(inputNumber)
+                .mapToInt(n -> n[1])
+                .reduce((a, b) -> b)
+                .orElse(0);
     }
 }
--- src/test/java/FibonacciNumberTest.java
+++ src/test/java/FibonacciNumberTest.java
@@ -1,22 +1,43 @@
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
 
 public class FibonacciNumberTest {
+    FibonacciNumber fibonacci;
+    @BeforeMethod
+    public void setUp(){
+        fibonacci = new FibonacciNumber();
+    }
 
     @DataProvider(name = "parameters")
     public static Object[][] fibonacciNumbers() {
         return new Object[][] {{0, 0}, {1, 1}, {2, 1}, {7, 13}, {16, 987}};
     }
 
+    @DataProvider(name = "negatives")
+    public static Object[][] negativeFibonacciNumbers() {
+        return new Object[][] {{-8, -21}, {-1, 1}, {-6, -8}, {-4, -3}};
+    }
+
     @Test(dataProvider = "parameters")
     public void shouldCalculateFibonacciNumberRecursively(int inputNumber, int expectedFibonacciNumber) {
-        FibonacciNumber fibonacci = new FibonacciNumber();
-        //System.out.println(inputNumber + " " + expectedResult);
         assertEquals(fibonacci.calculateRecursive(inputNumber),  expectedFibonacciNumber);
     }
 
+    @Test(dataProvider = "parameters")
+    public void shouldCalculateFibonacciNumberIteratively(int inputNumber, int expectedFibonacciNumber) {
+        assertEquals(fibonacci.calculateIterative(inputNumber),  expectedFibonacciNumber);
+    }
 
+    @Test(dataProvider = "parameters")
+    public void shouldCalculateFibonacciNumberUsingStreams(int inputNumber, int expectedFibonacciNumber) {
+        assertEquals(fibonacci.calculateStreams(inputNumber),  expectedFibonacciNumber);
+    }
 
+    @Test(dataProvider = "negatives")
+    public void shouldCalculateNegativeFibonacciNumberRecursively(int inputNumber, int expectedFibonacciNumber) {
+        assertEquals(fibonacci.calculateRecursive(inputNumber),  expectedFibonacciNumber);
+    }
 }
