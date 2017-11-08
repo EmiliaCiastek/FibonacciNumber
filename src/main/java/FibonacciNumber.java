85768b9c9756b449087381623ff7635940c9a695
emcia <emciastek@gmail.com>
2017-11-08 18:17:07 +0100

    Recursive method added

--- src/main/java/FibonacciNumber.java
+++ src/main/java/FibonacciNumber.java
@@ -1,2 +1,11 @@
 public class FibonacciNumber {
+    public int calculateRecursive(int inputNumber) {
+        if(inputNumber == 0) {
+            return 0;
+        } else if(inputNumber == 1){
+            return 1;
+        }
+
+        return calculateRecursive(inputNumber - 1)  + calculateRecursive(inputNumber - 2);
+    }
 }
--- src/test/java/FibonacciNumberTest.java
+++ src/test/java/FibonacciNumberTest.java
@@ -1,2 +1,22 @@
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
 public class FibonacciNumberTest {
+
+    @DataProvider(name = "parameters")
+    public static Object[][] fibonacciNumbers() {
+        return new Object[][] {{0, 0}, {1, 1}, {2, 1}, {7, 13}, {16, 987}};
+    }
+
+    @Test(dataProvider = "parameters")
+    public void shouldCalculateFibonacciNumberRecursively(int inputNumber, int expectedFibonacciNumber) {
+        FibonacciNumber fibonacci = new FibonacciNumber();
+        //System.out.println(inputNumber + " " + expectedResult);
+        assertEquals(fibonacci.calculateRecursive(inputNumber),  expectedFibonacciNumber);
+    }
+
+
+
 }
